71057d843bee7c7f2eae37f4be15f14e376eb818
owhslp <owhslp@icloud.com>
2024-01-08 21:44:42 +0100

    another changes

--- java.js
+++ java.js
@@ -1,18 +1,16 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+document.addEventListener("DOMContentLoaded", function() {
+  var coll = document.getElementsByClassName("collapsible");
 
+  for (var i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+          this.classList.toggle("active");
+          var content = this.nextElementSibling;
 
-
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-
-        } else {
-            content.style.display = "block";
-
-        }
-    });
-}
+          if (content.style.maxHeight) {
+              content.style.maxHeight = null;
+          } else {
+              content.style.maxHeight = content.scrollHeight + "px";
+          }
+      });
+  }
+});
